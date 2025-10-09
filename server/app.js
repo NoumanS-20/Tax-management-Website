7896e3c89c7cb5b035c7d1e2d809f63ef41f4762
Nouman Shafique <noumanahmed6860@gmail.com>
2025-10-09 20:14:00 +0530

    corrections_1

--- server/app.js
+++ server/app.js
@@ -73,10 +73,7 @@ app.use('*', (req, res) => {
 });
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/finstack-india', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/finstack-india')
   .then(() => {
     console.log('✅ Connected to MongoDB');
   })
